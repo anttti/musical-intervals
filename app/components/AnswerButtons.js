4928a0802ae4d68ca784a4afa61c8c7bcae2aff5
Antti Mattila <antti.mattila@futurice.com>
2016-03-06 12:39:24 +0200

    Make buttons full width

--- app/components/AnswerButtons.js
+++ app/components/AnswerButtons.js
@@ -5,107 +5,141 @@ import React, {
 import Button from "apsl-react-native-button";
 
 const AnswerButtons = ({ isDisabled, onPress }) => {
-    return <View style={styles.answerButtons}>
-        <Button
-            style={[styles.answerButton, styles.btn]}
-            onPress={() => onPress(0)}
-            isDisabled={isDisabled}>
-            Unison
-        </Button>
-        <Button
-            style={[styles.answerButton, styles.btn]}
-            onPress={() => onPress(1)}
-            isDisabled={isDisabled}>
-            Min 2nd
-        </Button>
-        <Button
-            style={[styles.answerButton, styles.btn]}
-            onPress={() => onPress(2)}
-            isDisabled={isDisabled}>
-            Maj 2nd
-        </Button>
-        <Button
-            style={[styles.answerButton, styles.btn]}
-            onPress={() => onPress(3)}
-            isDisabled={isDisabled}>
-            Min 3rd
-        </Button>
-        <Button
-            style={[styles.answerButton, styles.btn]}
-            onPress={() => onPress(4)}
-            isDisabled={isDisabled}>
-            Maj 3rd
-        </Button>
-        <Button
-            style={[styles.answerButton, styles.btn]}
-            onPress={() => onPress(5)}
-            isDisabled={isDisabled}>
-            Perfect 4th
-        </Button>
-        <Button
-            style={[styles.answerButton, styles.btn]}
-            onPress={() => onPress(6)}
-            isDisabled={isDisabled}>
-            Aug 4th
-        </Button>
-        <Button
-            style={[styles.answerButton, styles.btn]}
-            onPress={() => onPress(7)}
-            isDisabled={isDisabled}>
-            Perfect 5th
-        </Button>
-        <Button
-            style={[styles.answerButton, styles.btn]}
-            onPress={() => onPress(8)}
-            isDisabled={isDisabled}>
-            Min 6th
-        </Button>
-        <Button
-            style={[styles.answerButton, styles.btn]}
-            onPress={() => onPress(9)}
-            isDisabled={isDisabled}>
-            Maj 6th
-        </Button>
-        <Button
-            style={[styles.answerButton, styles.btn]}
-            onPress={() => onPress(10)}
-            isDisabled={isDisabled}>
-            Min 7th
-        </Button>
-        <Button
-            style={[styles.answerButton, styles.btn]}
-            onPress={() => onPress(11)}
-            isDisabled={isDisabled}>
-            Maj 7th
-        </Button>
-        <Button
-            style={[styles.answerButton, styles.btn]}
-            onPress={() => onPress(12)}
-            isDisabled={isDisabled}>
-            Octave
-        </Button>
+    return <View style={styles.buttons}>
+        <View style={styles.row}>
+            <View style={styles.buttonContainer}>
+                <Button
+                    style={styles.button}
+                    onPress={() => onPress(0)}
+                    isDisabled={isDisabled}>
+                    Unison
+                </Button>
+            </View>
+            <View style={styles.buttonContainer}>
+                <Button
+                    style={styles.button}
+                    onPress={() => onPress(1)}
+                    isDisabled={isDisabled}>
+                    Min 2nd
+                </Button>
+            </View>
+            <View style={styles.buttonContainer}>
+                <Button
+                    style={styles.button}
+                    onPress={() => onPress(2)}
+                    isDisabled={isDisabled}>
+                    Maj 2nd
+                </Button>
+            </View>
+        </View>
+        <View style={styles.row}>
+            <View style={styles.buttonContainer}>
+                <Button
+                    style={styles.button}
+                    onPress={() => onPress(3)}
+                    isDisabled={isDisabled}>
+                    Min 3rd
+                </Button>
+            </View>
+            <View style={styles.buttonContainer}>
+                <Button
+                    style={styles.button}
+                    onPress={() => onPress(4)}
+                    isDisabled={isDisabled}>
+                    Maj 3rd
+                </Button>
+            </View>
+            <View style={styles.buttonContainer}>
+                <Button
+                    style={styles.button}
+                    onPress={() => onPress(5)}
+                    isDisabled={isDisabled}>
+                    Perfect 4th
+                </Button>
+            </View>
+        </View>
+        <View style={styles.row}>
+            <View style={styles.buttonContainer}>
+                <Button
+                    style={styles.button}
+                    onPress={() => onPress(6)}
+                    isDisabled={isDisabled}>
+                    Aug 4th
+                </Button>
+            </View>
+            <View style={styles.buttonContainer}>
+                <Button
+                    style={styles.button}
+                    onPress={() => onPress(7)}
+                    isDisabled={isDisabled}>
+                    Perfect 5th
+                </Button>
+            </View>
+            <View style={styles.buttonContainer}>
+                <Button
+                    style={styles.button}
+                    onPress={() => onPress(8)}
+                    isDisabled={isDisabled}>
+                    Min 6th
+                </Button>
+            </View>
+        </View>
+        <View style={styles.row}>
+            <View style={styles.buttonContainer}>
+                <Button
+                    style={styles.button}
+                    onPress={() => onPress(9)}
+                    isDisabled={isDisabled}>
+                    Maj 6th
+                </Button>
+            </View>
+            <View style={styles.buttonContainer}>
+                <Button
+                    style={styles.button}
+                    onPress={() => onPress(10)}
+                    isDisabled={isDisabled}>
+                    Min 7th
+                </Button>
+            </View>
+            <View style={styles.buttonContainer}>
+                <Button
+                    style={styles.button}
+                    onPress={() => onPress(11)}
+                    isDisabled={isDisabled}>
+                    Maj 7th
+                </Button>
+            </View>
+        </View>
+        <View style={styles.row}>
+            <View style={styles.buttonContainer}>
+                <Button
+                    style={styles.button}
+                    onPress={() => onPress(12)}
+                    isDisabled={isDisabled}>
+                    Octave
+                </Button>
+            </View>
+        </View>
     </View>;
 };
 
 const styles = StyleSheet.create({
-    answerButtons: {
+    buttons: {
         flex: 0.6,
+        backgroundColor: "gray"
+    },
+    row: {
+        flex: 0.2,
         flexDirection: "row",
-        flexWrap: "wrap",
-        padding: 5
     },
-    answerButton: {
-        margin: 10,
-        width: 100,
-        height: 50,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#ff00ff"
+    buttonContainer: {
+        flex: 0.333,
     },
-    btn: {
+    button: {
+        flex: 1,
         backgroundColor: "#ff00ff",
         borderColor: "#ff00ff",
-        margin: 10
+        borderRadius: 0
     }
 });
 
